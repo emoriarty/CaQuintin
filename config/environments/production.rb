d1d8c318eff8fceb9d4704e361ffde28b4713989
= <=>
2011-12-24 18:58:53 +0100

    se modifica config/environemnts/production.rb fallo del copy/paste

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-MediathequeServer::Application.configure do
+CaQuintin::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
